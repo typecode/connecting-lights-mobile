8427d027d3c77f90fab9fd6cd2dbf8b0e8f2c2cf
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2012-08-23 16:38:55 -0400

    woah now

--- app/view/MessageContainer.js
+++ app/view/MessageContainer.js
@@ -12,64 +12,34 @@ Ext.define("connecting-lights-mobile.view.MessageContainer", {
         },
 
         listeners: {
-            deactivate: function(){
+            activate: function(){
                 this.setActiveItem(0);
+                this.message = Ext.create('connecting-lights-mobile.model.Message', {
+
+                });
+            },
+            deactivate: function(){
+
             }
         },
 
         items: [
 
-            {  //0
+            {
+                itemId: 'message',
                 xtype: 'container',
                 layout: 'fit',
                 items:[
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Location',
-                        items:[
-
-                        ]
-                    },
-                    geolocation_map,
-                    {
-                        xtype: 'button',
-                        text: 'Enter Message',
-                        height:'10%',
-                        ui: 'next',
-                        docked:'bottom',
-                        handler: function() {
-                            message_container.animateActiveItem(1, {type: 'slide', direction: 'left'});
-                        }
-                    }
-                ]
-            },
-
-            {  //1
-                xtype: 'container',
-                layout: 'fit',
-                items:[
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Message',
-                        items:[
-                            {
-                                xtype: 'button',
-                                ui: 'back',
-                                text: 'Back',
-                                handler: function() {
-                                    message_container.animateActiveItem(0, {type: 'slide', direction: 'right'});
-                                }
-                            }
-                        ]
+                        title: 'Write a Message'
                     },
                     {
                         xtype:'fieldset',
                         items:[
                             {
-                                xtype: 'textareafield',
-                                label: 'Message'
+                                xtype: 'textareafield'
                             }
                         ]
                         
@@ -80,48 +50,43 @@ Ext.define("connecting-lights-mobile.view.MessageContainer", {
                         ui: 'next',
                         docked:'bottom',
                         handler: function() {
-                            message_container.animateActiveItem(2, {type: 'slide', direction: 'left'});
+                            message_container.animateActiveItem(this.up('messagecontainer').getComponent('color'), {type: 'slide', direction: 'left'});
                         }
                     }
-                ]
+                ],
+                listeners: {
+                    activate: function(){
+
+                    },
+                    deactivate: function(){
+                        this.up('messagecontainer').message = this.up('messagecontainer').down('textareafield').getValue();
+                    }
+                }
             },
 
-            {  //2
+            {
+                itemId: 'color',
                 xtype: 'container',
                 layout: 'fit',
-                listeners: {
-                    activate: function() {
-                        var me;
-                        me = this;
-                        this.color_picker = new ColorPicker({
-                            $e: $('#color-picker'),
-                            src: 'resources/images/color-picker-HD.png'
-                        });
-                        this.color_picker.$e.bind('color:picked', function(){
-                            //console.log(me.element.up('.x-container', 3));
-                            me.element.up('.x-container',3).dom.style.backgroundColor = 'rgba(0,0,0,1)';
-                            
-                        });
-                    }
-                },
                 items: [
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Color',
+                        title: 'Color your Message',
                         items:[
                             {
                                 xtype: 'button',
                                 ui: 'back',
                                 text: 'Back',
                                 handler: function() {
-                                    message_container.animateActiveItem(1, {type: 'slide', direction: 'right'});
+                                    message_container.animateActiveItem(this.up('messagecontainer').getComponent('message'), {type: 'slide', direction: 'right'});
                                 }
                             }
                         ]
                     },
                     {
                         xtype: 'container',
+                        styleHtmlContent: true,
                         html: [
                             '<div id="color-picker" class="color-picker">',
                             '<div class="handle"></div>',
@@ -131,18 +96,80 @@ Ext.define("connecting-lights-mobile.view.MessageContainer", {
                     },
                     {
                         xtype: 'button',
-                        text: 'Send Message',
+                        text: 'Select a Location',
+                        ui: 'next',
+                        docked:'bottom',
+                        handler: function() {
+                            message_container.animateActiveItem(this.up('messagecontainer').getComponent('map'), {type: 'slide', direction: 'left'});
+                        }
+                    }
+                ],
+                listeners: {
+                    activate: function() {
+                        var me;
+                        me = this;
+                        if(!this.color_picker){
+                            this.color_picker = new ColorPicker({
+                                $e: $('#color-picker'),
+                                src: 'resources/images/color-picker-HD.png'
+                            });
+                            this.color_picker.$e.bind('color:picked', function(e, d){
+                                me.up('messagecontainer').message.r = d.r;
+                                me.up('messagecontainer').message.g = d.g;
+                                me.up('messagecontainer').message.b = d.b;
+                            });
+                        }
+                    }
+                }
+            },
+
+            {
+                itemId: 'map',
+                xtype: 'container',
+                layout: 'fit',
+                items:[
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Choose a Location',
+                        items:[
+                            {
+                                xtype: 'button',
+                                ui: 'back',
+                                text: 'Back',
+                                handler: function() {
+                                    message_container.animateActiveItem(this.up('messagecontainer').getComponent('color'), {type: 'slide', direction: 'right'});
+                                }
+                            }
+                        ]
+                    },
+                    geolocation_map,
+                    {
+                        xtype: 'button',
+                        text: 'Send your message!',
                         ui: 'confirm',
                         docked:'bottom',
                         handler: function() {
-                            message_container.animateActiveItem(3, {type: 'slide', direction: 'left'});
+                            message_container.animateActiveItem(this.up('messagecontainer').getComponent('thanks'), {type: 'slide', direction: 'left'});
                         }
                     }
-                ]
+                ],
+                listeners: {
+                    activate: function(){
+
+                    },
+                    deactivate: function(){
+                        var my_geo;
+                        my_geo = this.up('messagecontainer').down('geolocationmap').getGeo();
+                        this.up('messagecontainer').message.lat = my_geo._latitude;
+                        this.up('messagecontainer').message.lng = my_geo._longitude;
+                    }
+                }
             },
 
 
             {  //3
+                itemId: 'thanks',
                 xtype: 'container',
                 layout: 'fit',
                 items: [
@@ -153,11 +180,20 @@ Ext.define("connecting-lights-mobile.view.MessageContainer", {
                     },
                     {
                         xtype: 'container',
+                        styleHtmlContent: true,
                         html: [
                             '<p>Thanks!</p>'
                         ].join('')
                     }
-                ]
+                ],
+                listeners: {
+                    activate: function(){
+                        console.log(this.up('messagecontainer').message);
+                        this.up('messagecontainer').message.save();
+                    },
+                    deactivate: function()
+                    }
+                }
             }
         ]
     }

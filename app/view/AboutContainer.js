ae1c7e49b24c1363d4cf88d33ead7cec7f1e0524
tyler madsen <tylrmden@gmail.com>
2012-08-23 21:36:12 -0400

    about work

--- app/view/AboutContainer.js
+++ app/view/AboutContainer.js
@@ -1,13 +1,29 @@
 Ext.define("connecting-lights-mobile.view.AboutContainer", {
     extend: 'Ext.Container',
     xtype: 'Aboutcontainer',
+    requires: [
+        'Ext.Img'
+    ],
     config: {
         title: 'About',
         iconCls: 'info',
 
-        items: [
+        styleHtmlContent: false,
+        scrollable: {
+            direction: 'horizontal',
+            directionLock: true
+        },
+        layout: {
+            type: 'vbox',
+            align: 'stretch'
+        },
+
+        items:[
             {
-                docked: 'top'
+                xtype: 'container',
+                html: [
+                    '<img src="resources/images/about.png"></img>'
+                ].join('')
             }
         ]
     }
--- app/view/WelcomeContainer.js
+++ app/view/WelcomeContainer.js
@@ -31,12 +31,12 @@ Ext.define("connecting-lights-mobile.view.WelcomeContainer", {
             {
                 xtype: 'container',
                 html: [
-                    '<div>',
-                    '<p><img src="resources/images/logo.png"></img></p>',
-                    '<p><img src="resources/images/wall.jpeg"></img></p>',
-                    '<p><strong>Connecting Light</strong> is a digital art installation along Hadrian’s Wall World Heritage Site. The installation consists of hundreds of large-scale, light-filled balloons transmitting colors from one-to-another, creating a communication network spanning over seventy miles.</p>',
-                    '<p>The installation is open to the public from Friday, August 31st to Saturday, September 1st.</p>',
-                    '<p>View the full site <a href="http://connectinglights.com">here</a>.</p>',
+                    '<div class="welcome">',
+                    '   <h1><img src="resources/images/logo.png"></img></h1>',
+                    '   <p><img src="resources/images/wall.jpeg"></img></p>',
+                    '   <p><strong>Connecting Light</strong> is a digital art installation along Hadrian’s Wall World Heritage Site. The installation consists of hundreds of large-scale, light-filled balloons transmitting colors from one-to-another, creating a communication network spanning over seventy miles.</p>',
+                    '   <p>The installation is open to the public from Friday, August 31st to Saturday, September 1st.</p>',
+                    '   <p>View the full site <a href="http://connectinglights.com">here</a>.</p>',
                     '</div>'
                 ].join('')
             }

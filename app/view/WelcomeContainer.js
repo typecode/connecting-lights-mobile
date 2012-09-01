2573a0c6711d47a980e93a1f2c5d0fc749246e54
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2012-09-01 14:45:52 -0400

    fixed welcome link

--- app/view/WelcomeContainer.js
+++ app/view/WelcomeContainer.js
@@ -27,7 +27,7 @@ Ext.define("connecting-lights-mobile.view.WelcomeContainer", {
                     '   <div class="padding">',
                     '     <p><strong>Connecting Light</strong> is a digital art installation along Hadrian’s Wall World Heritage Site. The installation consists of hundreds of large-scale, light-filled balloons transmitting colors from one-to-another, creating a communication network spanning over seventy miles.</p>',
                     '    <p>The installation is open to the public from Friday, August 31st to Saturday, September 1st.</p>',
-                    '    <p>View the full site <a href="http://connectinglights.com">here</a>.</p>',
+                    '    <p>View the full site <a href="http://connectinglight.info">here</a>.</p>',
                     '   </div>',
                     '</div>'
                 ].join('')
